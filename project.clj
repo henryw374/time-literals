e7cdb9592c83f87a2ffae1175adf6958565268be
henryw374 <henryw374@gmail.com>
2018-07-20 16:07:12 +0100

    add url

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject jsr310-tagged-literals "0.1.0-SNAPSHOT"
   :description "Clojure(Script) tagged literals for jsr-310 entities"
+  :url "https://github.com/henryw374/jsr310-tagged-literals"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
@@ -8,5 +9,4 @@
             {:source-paths ["dev"]
              :dependencies [[org.clojure/clojure "1.9.0"]
                             [org.clojure/clojurescript "1.10.238"]]
-             :plugins [[lein-doo "0.1.10"]]
-             :jvm-opts ["-Dclojure.spec.compile-asserts=true"]}})
+             :plugins [[lein-doo "0.1.10"]]}})
