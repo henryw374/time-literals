89b0e67f459aff9a072266722484d7293c3fd041
henryw374 <henryw374@gmail.com>
2018-08-22 08:14:45 +0100

    bump dependency on cljs.java-time

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject jsr310-tagged-literals "0.1.2"
+(defproject jsr310-tagged-literals "0.1.3"
   :description "Clojure(Script) tagged literals for jsr-310 entities"
   :url "https://github.com/henryw374/jsr310-tagged-literals"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cljs.java-time "0.1.0"]]
+  :dependencies [[cljs.java-time "0.1.2"]]
   :profiles {:dev
             {:source-paths ["dev"]
              :dependencies [[org.clojure/clojure "1.9.0"]
